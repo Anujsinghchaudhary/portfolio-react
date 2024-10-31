bd92ff2c5b63e41c7a945156fb3c7ef6879ab955
AnujSingh07 <anujsinghchaudhary07@gmail.com>
2024-10-31 14:27:03 +0530

    social_links added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -22,15 +22,15 @@ const About = () => {
           <div className="about-skills">
             <div className="about-skill">
               <p>HTML & CSS</p>
-              <hr style={{ width: "50%" }} />
+              <hr style={{ width: "60%" }} />
             </div>
             <div className="about-skill">
               <p>JavaScript</p>
-              <hr style={{ width: "70%" }} />
+              <hr style={{ width: "60%" }} />
             </div>
             <div className="about-skill">
               <p>React</p>
-              <hr style={{ width: "60%" }} />
+              <hr style={{ width: "80%" }} />
             </div>
             <div className="about-skill">
               <p>Node.js</p>
