0da04b8c911e3ba490d71a074ab172d88b63107a
AnujSingh07 <anujsinghchaudhary07@gmail.com>
2024-11-07 18:45:20 +0530

    added profile card

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,11 +3,121 @@ import './Hero.css';
 import Profile_anuj from "../../assets/Profile_anuj.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import social_links from "../../api/social_links.json";
+import styled from 'styled-components';
+
+const Card = () => { 
+  return (
+    <StyledWrapper>
+      <div className="card">
+        <div className="blob" />
+        <img className="img" src={Profile_anuj} alt="Profile" />
+        <h2>Hi, I am Anuj<br /><span>Singh</span></h2>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  .card {
+    width: 500px;
+    height: 300px; 
+    background: #f0f0f0;
+    border-radius: 10px;
+    text-align: center;
+    transition: all 0.5s;
+    position: relative;
+  }
+
+
+  .card:hover {
+    box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.623);
+    background-color: #4bb8ff;
+  }
+
+  .card .blob {
+    height: 10px;
+    width: 75%;
+    border-radius: 0 0 30px 30px;
+    margin: 0 auto;
+    background-color: #4bb8ff;
+    visibility: visible;
+    transition: all 0.3s;
+  }
+
+  .card:hover .blob {
+    height: 0;
+  }
+
+  .card .img {
+    display: flex;
+    margin: 30px auto 10px auto;
+    width: 70px;
+    height: 70px;
+    background-color: #4bb8ff;
+    border-radius: 50%;
+    font-size: 11px;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.5s;
+  }
+
+  .card:hover .img {
+    width: 100%;
+    height: 70%;
+    border-radius: 10px 0 0;
+    margin: 0 auto;
+    background-color: #f0f0f0;
+    z-index: 99999;
+  }
+
+  .card h2 {
+    padding: 15px 10px;
+    font-size: 25px;
+    transition: all 0.1s;
+    z-index: -99;
+    line-height: 17px;
+  }
+
+  .card span {
+    font-size: 18px;
+  }
+
+  .card:hover h2 {
+    opacity: 0;
+    width: 100%;
+    position: absolute;
+    transition: all 0.5s;
+  }
+
+  .card > p {
+    opacity: 0;
+    transition: all 0.75s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .card > p > svg {
+    padding: 5px;
+  }
+
+  .card:hover > p {
+    position: absolute;
+    bottom: 15px;
+    left: 0;
+    right: 0;
+    opacity: 1;
+    transition: all 0.1s;
+  }
+`;
+
+
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
+    <div id='home' className='hero'><Card>
       <img src={Profile_anuj} alt="Profile_anuj" width="300px" height="300px"/>
-      <h1><span>I am Anuj Singh</span></h1>
+      </Card>
       <p>Full Stack Developer | Problem Solver | Learner</p>
       <div className="hero-action">
         <div className="hero-contact">
