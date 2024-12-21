12ded2be0977559e643c879ccef8272358c43f97
AnujSingh07 <anujsinghchaudhary07@gmail.com>
2024-12-21 14:03:11 +0530

    smooth effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Navbar from './components/navbar/navbar'
-import Hero from './components/Hero/Hero'
-import Contact from './components/contact/contact'
-import Footer from './components/footer/footer'
-import SkillsSection  from './components/skills/SkillsSection'
-import Aboutme from './components/aboutme/AboutMe'
-import Projects  from './components/projects/Projects'
+import React, { useEffect } from 'react';
+import Navbar from './components/navbar/navbar';
+import Hero from './components/Hero/Hero';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import SkillsSection from './components/skills/SkillsSection';
+import Aboutme from './components/aboutme/AboutMe';
+import Projects from './components/projects/Projects';
+import './smoothScroll';
+
 const App = () => {
   return (
     <div className="bg-gray-900 min-h-screen">
@@ -19,7 +21,7 @@ const App = () => {
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
